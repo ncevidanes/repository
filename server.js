d570a0b2f08a45778998d76657cde2354f0083e8
Nelson Assis <153871595+ncevidanes@users.noreply.github.com>
2026-04-27 15:37:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,10 @@ app.post('/api/repository/simulations', upload.single('simulation_file'), async 
     const fileStream = fs.createReadStream(req.file.path);
     await axios.put(`${bucketUrl}/${req.file.originalname}`, fileStream, {
       params: { access_token: process.env.ZENODO_TOKEN },
-      headers: { 'Content-Type': 'application/octet-stream' }
+      headers: { 
+        'Content-Type': 'application/octet-stream',
+        'Content-Length': req.file.size //
+      }
     });
 
     // Passo C: Salva o registro no seu MongoDB com o ID oficial do CERN
